91c27d3e2e5e5c75775c4b11e662257ebe59eff2
Zehvogel <4reichen@informatik.uni-hamburg.de>
2020-12-03 18:07:45 +0100

    first 2 days

---
+++ 1/solution.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def part1(data):
+    for i in range(data.size):
+        for j in range(i+1, data.size):
+            tmp = data[i] + data[j]
+            if tmp == 2020:
+                return data[i] * data[j]
+
+def part2(data):
+    for i in range(data.size):
+        for j in range(i+1, data.size):
+            for k in range(j+1, data.size):
+                tmp = data[i] + data[j] + data[k]
+                if tmp == 2020:
+                    return data[i] * data[j] * data[k]
+
+rawdata = []
+with open("input") as f:
+    rawdata = f.read()
+
+data = np.asarray(rawdata.split(), dtype=np.uint)
+
+print(part1(data))
+print(part2(data))
---
+++ 2/solution.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import re
+
+rawdata = []
+with open("input") as f:
+    rawdata = f.read()
+
+pattern = re.compile(r"(\d+)-(\d+) (\w): (\w+)")
+
+matches = pattern.finditer(rawdata)
+
+part1_valid = 0
+part2_valid = 0
+
+for match in matches:
+    param1 = int(match.group(1))
+    param2 = int(match.group(2))
+    char = match.group(3)
+    password = match.group(4)
+    count = password.count(char)
+    if (count >= param1 and count <= param2):
+        part1_valid += 1
+    if (bool(password[param1 - 1] == char) ^ bool(password[param2 - 1] == char)):
+        part2_valid += 1
+
+print(part1_valid)
+print(part2_valid)
