41acda7354115cc4d247a96da151f2f214050df7
Zehvogel <4reichen@informatik.uni-hamburg.de>
2020-12-05 17:03:17 +0100

    lol

--- 5/part1.sh
+++ 5/part1.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
 
-# would be nice to get this in one line :(
-NUM="2#"$(cat input | tr BF 10 | tr RL 10 | sort | tail -n 1)
-echo $(($NUM))
+echo $(("2#"$(cat input | tr BF 10 | tr RL 10 | sort | tail -n 1)))
