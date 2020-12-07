ab1a5ab3985aa23583390915f9fb9206ab375df8
Zehvogel <4reichen@informatik.uni-hamburg.de>
2020-12-07 20:16:33 +0100

    day 6

---
+++ 6/solution.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+rawdata = []
+with open("input") as f:
+    rawdata = f.read()
+
+data = rawdata.split("\n\n")
+
+any_count = 0
+all_count = 0
+for group in data:
+    # probably a bit overkill lol
+    any_yes = {c for c in group.replace("\n", "")}
+    any_count += len(any_yes)
+    all_yes = any_yes
+    for line in group.split():
+        all_yes = all_yes.intersection({c for c in line})
+    all_count += len(all_yes)
+
+print(any_count)
+print(all_count)
